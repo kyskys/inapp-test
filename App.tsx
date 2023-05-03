aeff5e39ebaf818b458c2e35ff5b8a15756ae8f6
kyskys <yoo99485@modernlion.io>
2023-05-03 17:52:35 +0900

    :sparkles: ADD: initial commit

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 /**
- * Sample React Native App
+ * Sample React Native App/images/dody_/post/51b9838e-2664-4550-8d79-03186decbcc1/Screen Shot 2020-07-22 at 12.44.17 PM.png
  * https://github.com/facebook/react-native
  *
  * @format
@@ -17,7 +17,7 @@ function App(): JSX.Element {
     webviewRef.current = _ref;
   };
 
-  //웹뷰에서 rn으로 값을 보낼때 함수
+  //웹뷰에서 RN으로 메시지를 보냈을 떄 실행되는 함수
   const handleOnMessage = (e: {nativeEvent: {data: string}}) => {
     // postMessage 담겨있는 결과 값이 웹뷰에서 값을 출력
     console.log('콘솔', e.nativeEvent.data);
@@ -28,7 +28,9 @@ function App(): JSX.Element {
     <View style={{flex: 1}}>
       <WebView
         onMessage={handleOnMessage}
-        source={{uri: 'http://localhost:3001/originals/medi-society'}}
+        source={{
+          uri: 'http://localhost:3000/originals/buwoo-buwoo/checkout?ordNo=01GZE599943ATN6Q1KHBP0R2TA&metadataKey=euy3aiSh',
+        }}
         ref={handleSetRef}
       />
     </View>
